351ff871134530283893b52ec58b20bf70359020
Andrei Pietrușel <andreipietrusel@gmail.com>
2016-04-08 01:16:40 +0300

    Strings learned.

---
+++ src/main/java/com/accenture/andreipietrusel/scjp6playground/formattingparsingapi/StringExperiments.java
@@ -0,0 +1,23 @@
+package com.accenture.andreipietrusel.scjp6playground.formattingparsingapi;
+
+public class StringExperiments {
+
+	public static void main(String[] args) {
+		String s1 = "ab";
+		String s2 = "cd";
+		String s = s1.concat(s2);
+		String sp = "abcd";
+		System.out.println(s == sp);
+		
+		s.intern(); // already in pool, doesnt replace it the pool one, returns old pooled object
+		System.out.println(s == sp); // this will be false
+		
+		String rev = s2.concat(s1);
+		rev.intern(); // adds exactly this string to pool, returns same (not used here)
+		System.out.println("cdab" == rev); // this will be true
+		
+		String rev2 = new String(rev); // String(String) copy constructor
+		System.out.println(rev == rev2); // false
+	}
+	
+}
