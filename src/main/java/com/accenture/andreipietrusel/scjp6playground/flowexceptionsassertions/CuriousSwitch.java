6743c45fccadb34fc957ca757983c17d1067d7ef
Andrei Pietrușel <andrei.pietrusel@accenture.com>
2016-04-22 15:06:35 +0300

    Inner classes chapter done.

--- src/main/java/com/accenture/andreipietrusel/scjp6playground/flowexceptionsassertions/CuriousSwitch.java
+++ src/main/java/com/accenture/andreipietrusel/scjp6playground/flowexceptionsassertions/CuriousSwitch.java
@@ -12,7 +12,12 @@ public class CuriousSwitch {
 			System.out.print("collie ");
 			/* XXX: It's default, not case default: */
 			// case default:
+			int a = 3;
+			break;
 		default:
+			/* XXX: variables declared in prev cases are reusable evn >break */
+			/* XXX: previous case vars must be reinitialized explicitly */
+			// System.out.println(a); must be reinitialized in order to work
 			System.out.print("retriever ");
 		case harrier:
 			System.out.print("harrier ");
