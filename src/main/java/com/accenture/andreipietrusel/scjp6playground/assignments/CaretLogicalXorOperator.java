fe61c75ce7424cdfcbdd5cfe5ea718ef6c550089
Andrei Pietrușel <andrei.pietrusel@accenture.com>
2016-04-26 18:10:36 +0300

    Assignments chapter done!

--- src/main/java/com/accenture/andreipietrusel/scjp6playground/assignments/CaretLogicalXorOperator.java
+++ src/main/java/com/accenture/andreipietrusel/scjp6playground/assignments/CaretLogicalXorOperator.java
@@ -11,6 +11,16 @@ public class CaretLogicalXorOperator {
 		if ((distance + 1 != tail) ^ ((story * 2) == distance))
 			System.out.print("2"); // false ^ false = false
 		/* No output is produced. */
+		
+		/* XXX: Additive inverse of short value -128 is -128. */
+		byte z = Byte.MIN_VALUE;
+		byte zz = (byte) -z;
+		System.out.println(z + " " + zz);
+		
+		/* XXX: Applying - or + unary operator converts a value < int to int */
+		byte a = -2;
+		byte b =(byte) +a;
+		System.out.println(b);
 	}
 
 }
--- src/main/java/com/accenture/andreipietrusel/scjp6playground/objectorientation/curiosity/MultipleInterfaceConstantInheritance.java
+++ src/main/java/com/accenture/andreipietrusel/scjp6playground/objectorientation/curiosity/MultipleInterfaceConstantInheritance.java
@@ -13,6 +13,15 @@ public class MultipleInterfaceConstantInheritance implements I1, I2 {
 	/* XXX: Two impl. interf. have same const &u use it => ambig. comp. err */
 	public static void main(String... smt) {
 		// System.out.println("String is " + s); // This would give c.err
+
+		String a = "abc";
+		String b = "efg";
+
+		String d = "abcefg";
+		String c = a + b;
+//		c = c.intern();
+
+		System.out.println((c == d) + " " + c + " " + d);
 	}
 
 }
