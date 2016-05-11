7483c5a09b9db75dd50708270ce47612c4d4c947
Andrei Pietrușel <andrei.pietrusel@accenture.com>
2016-05-11 11:09:42 +0300

    Code examples++.

---
+++ src/main/java/com/accenture/andreipietrusel/scjp6playground/innerclasses/InnerClassSameNameAsParent.java
@@ -0,0 +1,10 @@
+package com.accenture.andreipietrusel.scjp6playground.innerclasses;
+
+public class InnerClassSameNameAsParent {
+
+	/* XXX: Cannot have inner class with same name as parent. Not even static */
+//	public class InnerClassSameNameAsParent {
+//
+//	}
+
+}
